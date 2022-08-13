f86b071f788aa1ab88f8f92e28084a07853f0f4d
Nico van den Brink <nico@maxilia.nl>
2022-08-13 22:26:08 +0200

    finished first version

---
+++ src/components/Experience.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { fly } from '../transitions'
+    import { inview } from 'svelte-inview'
+
+    export let
+        item = {},
+        delay = 0
+
+    let inView = false
+</script>
+
+<div class="flex justify-center w-full" class:min-h-screen={!inView}
+     on:enter|once={() => inView = true}
+     use:inview
+>
+    {#if inView}
+        <div class="grid grid-cols-4 gap-8 w-full xl:w-2/3 pl-4" transition:fly={{ x: '75%', delay }}>
+            <div class="flex flex-col text-left xl:text-right text-gray-900 uppercase -mt-1.5 text-xs xl:text-base">
+                {#if item.level}
+                    <p>{item.level}</p>
+                {/if}
+                <p class="whitespace-pre-line">{item.title}</p>
+                <p class="text-xxs xl:text-sm opacity-60">{item.period}</p>
+            </div>
+            <div class="flex relative flex-col col-span-3 pl-8 pr-4 pb-8 border-l border-gray-200">
+                <div class="absolute -top-2 -left-3">
+                    <img alt="{item.logo}" class="py-1 w-6 bg-white" loading="lazy" src="images/{item.logo}">
+                </div>
+                <div class="-mt-1.5 space-y-1 relative">
+                    <div class="flex items-center space-x-1">
+                        <p class="font-medium text-blue-600">{`${item.place}, ${item.location}`}</p>
+                    </div>
+                    <p class="whitespace-pre-line">{item.description}</p>
+                </div>
+            </div>
+        </div>
+    {/if}
+</div>
---
+++ src/components/Skills.svelte
@@ -0,0 +1,115 @@
+<script>
+    import { fly } from '../transitions'
+    import { inview } from 'svelte-inview'
+
+    const skills = [
+        {
+            title: 'Laravel/PHP',
+            logo: 'laravel.png',
+            rating: 80,
+        },
+        {
+            title: 'C#',
+            logo: 'csharp.png',
+            rating: 70,
+        },
+        {
+            title: 'Java',
+            logo: 'java.png',
+            rating: 50,
+        },
+        {
+            title: 'Javascript',
+            logo: 'javascript.png',
+            rating: 90,
+        },
+        {
+            title: 'Typescript',
+            logo: 'typescript.png',
+            rating: 75,
+        },
+        {
+            title: 'HTML',
+            logo: 'html.png',
+            rating: 95,
+        },
+        {
+            title: 'CSS',
+            logo: 'css.png',
+            rating: 85,
+        },
+        {
+            title: 'Tailwind',
+            logo: 'tailwind.png',
+            rating: 95,
+        },
+        {
+            title: 'Angular',
+            logo: 'angular.png',
+            rating: 70,
+        },
+        {
+            title: 'Svelte',
+            logo: 'svelte.png',
+            rating: 90,
+        },
+        {
+            title: 'Github',
+            logo: 'github.png',
+            rating: 85,
+        },
+        {
+            title: 'Bitbucket',
+            logo: 'bitbucket.png',
+            rating: 95,
+        },
+        {
+            title: 'Jira',
+            logo: 'jira.png',
+            rating: 90,
+        },
+        {
+            title: 'Jenkins',
+            logo: 'jenkins.png',
+            rating: 60,
+        },
+        {
+            title: 'SQL',
+            logo: 'sql.png',
+            rating: 65,
+        },
+        {
+            title: 'GraphQL',
+            logo: 'graphql.png',
+            rating: 80,
+        },
+        {
+            title: 'RavenDB',
+            logo: 'ravendb.png',
+            rating: 60,
+        },
+    ]
+
+    let inView = false
+</script>
+
+<div class="flex flex-col space-y-4"
+     class:min-h-screen={!inView}
+     on:enter|once={() => inView = true}
+     use:inview
+>
+    {#if inView}
+        {#each skills as skill, index}
+            <div class="space-y-2" transition:fly={{ x: '75%', delay: index * 100 }}>
+                <div class="flex items-center space-x-4">
+                    <img class="w-6 rounded-sm" src="images/{skill.logo}" alt={skill.logo} loading="lazy">
+                    <p>{skill.title}</p>
+                </div>
+                <div class="relative self-stretch h-1">
+                    <div class="absolute left-0 w-full h-full bg-gray-100"></div>
+                    <div class="absolute left-0 h-full bg-blue-600" style="width: {skill.rating}%"></div>
+                </div>
+            </div>
+        {/each}
+    {/if}
+</div>
